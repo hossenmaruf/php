b398ee13ce635fbde487b5a5a8ecc05fb35a9960
hossenmaruf <severusmaruf740@gmail.com>
2022-08-09 02:47:24 +0600

    php

---
+++ basic.php
@@ -0,0 +1,27 @@
+<?php
+
+
+  echo "hello world" ;
+
+
+   $a = 10 ;
+   $b = 20 ;
+   $c = 30 ;
+
+
+
+      if ($a > $b) {
+        echo "a is greater" ;
+      } 
+      else {
+        echo "not" ;
+      }
+
+  
+   
+
+
+
+
+
+?>
